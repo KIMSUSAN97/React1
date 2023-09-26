208f5d1143fb043f06b9f317c026a2f8fb167cc8
KIMSUSAN97 <kagamine9780@naver.com>
2023-09-26 20:13:02 +0900

    코딩애플실습

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button, Navbar, Container, Nav } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+import data from './data';
+import { Routes, Route, Link } from 'react-router-dom'
+import Detail from './Detail';
 
 function App() {
+
+  let [shoes] = useState(data);
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">REACT SHOP</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Detail</Nav.Link>
+            <Link to="/">홈</Link>
+            <Link to="/detail">상세페이지</Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+<Routes>
+  <Route path="/" element={
+      <div>
+      <div className="main-bg"></div>
+      <div className="container">
+        <div className="row">
+          {
+            shoes.map(function(a, i){
+                return (
+                  <Card shoes={shoes[i]} i={i+1}></Card>
+                )
+            })
+          }
+        </div>
+      </div>
+    </div>
+  } />
+  <Route path="/detail" element={<div>{ <Detail></Detail> }</div>} />
+  <Route path="/about" element={<div>어바웃페이지</div>} />
+</Routes>
     </div>
   );
 }
+
+function Card(props) {
+  return (
+    <div className="col-md-4">
+    <img src={'https://codingapple1.github.io/shop/shoes'+props.i+'.jpg'} width="80%"/>
+    <h4>{props.shoes.title}</h4>
+    <p>{props.shoes.price}</p>
+    </div>
+  )
+}
+
 
 export default App;
