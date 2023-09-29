8d3a1937b12a6d7d341a78f3d22fb4747885f509
KIMSUSAN97 <kagamine9780@naver.com>
2023-09-29 19:05:21 +0900

    9/29

--- src/routes/Cart.js
+++ src/routes/Cart.js
@@ -1,30 +1,43 @@
 import {Table} from 'react-bootstrap'
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { changeName, increase } from '../store';
 
 function Cart(){
 
-    let a = useSelector((state)=>{ return state.user })
-    console.log(a)
+    let state = useSelector((state) => state )
+    let dispatch = useDispatch()
 
 
     return (
     <div> 
+    <h6>{state.user.name} {state.user.age}의 장바구니</h6>
+    <button onClick={() => {dispatch(increase(1))}}>버튼</button>
     <Table>
     <thead>
-      <tr>s
-        <th>#</th>
+      <tr>
         <th>상품명</th>
         <th>수량</th>
         <th>변경하기</th>
+        <th></th>
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <td>1</td>
-        <td>안녕</td>
-        <td>안녕</td>
-        <td>안녕</td>
-      </tr>
+      {
+        state.cart.map((a, i) => {
+          return (
+            <tr>
+              <td>1</td>
+              <td>{state.cart[i].name}</td>
+              <td>{state.cart[i].count}</td>
+              <td>
+                <button onClick={()=>{
+                  dispatch(changeName())
+                }}>+</button>
+              </td>
+            </tr>
+          )
+        })
+      }
     </tbody>
     </Table> 
     </div>
--- src/store.js
+++ src/store.js
@@ -2,18 +2,34 @@ import { configureStore, createSlice } from '@reduxjs/toolkit'
 
 let user = createSlice({
     name : 'user',
-    initialState : 'kim'
+    initialState : { name : 'kim', age : 20 },
+    reducers : {
+      changeName(state){
+        state.name = 'park'
+      },
+      increase(state, action){
+        state.age += action.payload
+      },
+      
+
+    }
+
 })
 
-let stock = createSlice({
-    name : 'stock',
-    initialState : [10, 11, 12]
+export let { changeName, increase } = user.actions
+
+let cart = createSlice({
+  name : 'cart',
+  initialState : [
+    {id : 0, name : 'White and Black', count : 2},
+    {id : 2, name : 'Grey Yordan', count : 1}
+  ]
 })
 
 
 export default configureStore({
   reducer: {   
     user : user.reducer,
-    stock : stock.reducer
+    cart : cart.reducer
   }
 }) 
